880c68f65317eeba8a4f8340b9b6a1517bf076b3
Hocini Oualid <hocini.oualid@gmail.com>
2015-07-09 16:54:18 +0200

    new fichier core

---
+++ test.core.class.php
@@ -0,0 +1,36 @@
+<?php
+class test extends bdd{
+		
+	public $value;
+	public $return;
+	public $article = [  "id" => "",
+						"titre" => "",
+						"contenu" => "",
+						"statut" => "",
+						"meta_title" => "",
+						"meta_description" => "",
+						"date_publication" => "",
+						"date_last_modification" => "",
+						"type_page" => "",
+						"membre_idmembre" => "",
+						"article_url" => ""
+	];
+	
+	public function __construct() {
+		parent::__construct();
+
+		$this->url = explode("/", trim($_SERVER['REQUEST_URI'], "/"));
+		$this->value = $this->url[0];
+	}
+	
+	public function testArticle(){
+		$this->getOneBy($this->value, "id", "article", "page");
+		if (isset($this->article["id"])){
+			return TRUE;
+		}else{
+			return FALSE;
+		} 
+	}
+	
+}
+?>
